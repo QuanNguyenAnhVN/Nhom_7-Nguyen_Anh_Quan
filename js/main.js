2106dc76eb4f5aa009d4a07a51ace298330db60f
TessTickle <quan.nguyenanh304@gmail.com>
2022-06-27 22:15:48 +0700

    27/6/2022

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// home = document.getElementById("home");
+// contact = document.getElementById("contact");
+
+function moveHome() {
+    document.getElementById("home").style.display = "grid";
+    document.getElementById("contact").style.display = "none";
+}
+
+function moveContact() {
+    document.getElementById("contact").style.display = "grid";
+    document.getElementById("home").style.display = "none";
+}
+
