48da8a1a9de6be4c47b180f8b912c2b927b08c32
TessTickle <quan.nguyenanh304@gmail.com>
2022-06-29 00:15:21 +0700

    29/6/2022

--- js/main.js
+++ js/main.js
@@ -1,13 +1,125 @@
-// home = document.getElementById("home");
-// contact = document.getElementById("contact");
+home = document.getElementById("home");
+portfolio = document.getElementById("portfolio");
+contact = document.getElementById("contact");
 
+// Navbar Selection ----------------------------------------------
 function moveHome() {
-    document.getElementById("home").style.display = "grid";
-    document.getElementById("contact").style.display = "none";
+    home.style.display = "grid";
+    portfolio.style.display = "none"
+    contact.style.display = "none";
+}
+
+function movePortfolio() {
+    portfolio.style.display = "grid"
+    contact.style.display = "none";
+    home.style.display = "none";
+    
 }
 
 function moveContact() {
-    document.getElementById("contact").style.display = "grid";
-    document.getElementById("home").style.display = "none";
+    contact.style.display = "grid";
+    home.style.display = "none";
+    portfolio.style.display = "none"
 }
+
+
+
+// Portfolio Selection ------------------------------------------
+const Poland = document.querySelectorAll('#p2, #p3');
+const France = document.querySelectorAll('#p1, #p4');
+const Belgium = document.querySelectorAll('#p5, #p6');
+const Denmark = document.querySelectorAll('#p7, #p8');
+
+function select_0(){
+    for (let i = 0; i < Poland.length; i++) {
+        Poland[i].style.display = "block";
+    }
+    
+    for (let i = 0; i < France.length; i++) {
+        France[i].style.display = "block";
+    }
+
+    for (let i = 0; i < Belgium.length; i++) {
+        Belgium[i].style.display = "block";
+    }
+
+    for (let i = 0; i < Denmark.length; i++) {
+        Denmark[i].style.display = "block";
+    }
+}
+
+function select_1(){
+    for (let i = 0; i < Poland.length; i++) {
+        Poland[i].style.display = "block";
+    }
+
+    for (let i = 0; i < France.length; i++) {
+        France[i].style.display = "none";
+    }
+
+    for (let i = 0; i < Belgium.length; i++) {
+        Belgium[i].style.display = "none";
+    }
+
+    for (let i = 0; i < Denmark.length; i++) {
+        Denmark[i].style.display = "none";
+    }
+}
+
+function select_2(){
+    for (let i = 0; i < Poland.length; i++) {
+        Poland[i].style.display = "none";
+    }
+
+    for (let i = 0; i < France.length; i++) {
+        France[i].style.display = "block";
+    }
+
+    for (let i = 0; i < Belgium.length; i++) {
+        Belgium[i].style.display = "none";
+    }
+
+    for (let i = 0; i < Denmark.length; i++) {
+        Denmark[i].style.display = "none";
+    }
+}
+
+function select_3(){
+    for (let i = 0; i < Poland.length; i++) {
+        Poland[i].style.display = "none";
+    }
+
+    for (let i = 0; i < France.length; i++) {
+        France[i].style.display = "none";
+    }
+
+    for (let i = 0; i < Belgium.length; i++) {
+        Belgium[i].style.display = "block";
+    }
+
+    for (let i = 0; i < Denmark.length; i++) {
+        Denmark[i].style.display = "none";
+    }
+}
+
+function select_4(){
+    for (let i = 0; i < Poland.length; i++) {
+        Poland[i].style.display = "none";
+    }
+
+    for (let i = 0; i < France.length; i++) {
+        France[i].style.display = "none";
+    }
+
+    for (let i = 0; i < Belgium.length; i++) {
+        Belgium[i].style.display = "none";
+    }
+
+    for (let i = 0; i < Denmark.length; i++) {
+        Denmark[i].style.display = "block";
+    }
+}
+
+
+
 
