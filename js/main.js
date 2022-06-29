78b1bcc08b13b5f407233f3bb66aeca7b384821b
TessTickle <quan.nguyenanh304@gmail.com>
2022-06-29 19:05:22 +0700

    7:05 29/6/2022

--- js/main.js
+++ js/main.js
@@ -1,25 +1,41 @@
 home = document.getElementById("home");
+about = document.getElementById("about");
 portfolio = document.getElementById("portfolio");
 contact = document.getElementById("contact");
-
+All_nav = document.querySelectorAll("#home, #about, #portfolio, #contact");
 // Navbar Selection ----------------------------------------------
 function moveHome() {
-    home.style.display = "grid";
-    portfolio.style.display = "none"
-    contact.style.display = "none";
+   for (let i=0; i <All_nav.length;i++){
+        if(All_nav[i] == home){
+            All_nav[i].style.display = "grid";
+        } else All_nav[i].style.display = "none";
+   }
+}
+
+function moveAbout() {
+    for (let i=0; i <All_nav.length;i++){
+        if(All_nav[i] == about){
+            All_nav[i].style.display = "grid";
+        } else All_nav[i].style.display = "none";
+   }
+
 }
 
 function movePortfolio() {
-    portfolio.style.display = "grid"
-    contact.style.display = "none";
-    home.style.display = "none";
+    for (let i=0; i <All_nav.length;i++){
+        if(All_nav[i] == portfolio){
+            All_nav[i].style.display = "grid";
+        } else All_nav[i].style.display = "none";
+   }
     
 }
 
 function moveContact() {
-    contact.style.display = "grid";
-    home.style.display = "none";
-    portfolio.style.display = "none"
+    for (let i=0; i <All_nav.length;i++){
+        if(All_nav[i] == contact){
+            All_nav[i].style.display = "grid";
+        } else All_nav[i].style.display = "none";
+   }
 }
 
 
@@ -32,16 +48,12 @@ const Denmark = document.querySelectorAll('#p7, #p8');
 const All = document.querySelectorAll('#p1, #p2, #p3, #p4, #p5, #p6 , #p7, #p8')
 
 function select_0(){
-   
-   
     for (let i = 0; i < All.length; i++) {
         All[i].style.display = "block";
     }
-
 }
 
 function select_1(){
-    
     for(let i = 0; i < All.length; i++){
         for(let n = 0;n < Poland.length;n++){
             if (All[i] != Poland[n]){
@@ -49,7 +61,6 @@ function select_1(){
             } else All[i].style.display = "block";
         }
     }
-
 }
 
 function select_2(){
